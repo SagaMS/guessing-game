4fca41ca2bb22821e2b5d98c0c826bcbce2fd9bf
SagaMS <91miablond@gmail.com>
2024-10-25 14:23:43 +0700

    feat:solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.number = 0;
+  
+    }
+  
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+  
     }
-
+  
     guess() {
-
+       this.number = Math.ceil((this.max+this.min)/2);
+       return this.number;
+        
     }
-
+  
     lower() {
-
+        //returned number which is greater than answer
+        this.max = this.number;
+  
+  
     }
-
+  
     greater() {
-
+        this.min = this.number;
     }
-}
+  }
 
 module.exports = GuessingGame;
